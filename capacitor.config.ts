7c48d78e109275d2f5be50c30d04229144f80a7d
dor <dor@ut.com>
2025-07-20 12:19:28 +0300

    2.6

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.barf.finance',
+  appName: 'barf-finance',
+  webDir: 'dist',
+};
+
+export default config;
