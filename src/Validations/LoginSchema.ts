e7f8d674d960f699f603cf3a577ca3e7ace9b778
dor <dor@ut.com>
2025-08-14 23:53:01 +0300

    4.1

--- src/Validations/LoginSchema.ts
+++ src/Validations/LoginSchema.ts
@@ -1,10 +1,26 @@
+// Validations/LoginSchema.ts
 import joi from "joi";
 
 export const LoginSchema = joi.object({
-    email: joi.string().email({tlds: {allow: false}}),
-    password: joi.string()
-    .ruleset.pattern(/((?=.*\d{1})(?=.*[A-Z]{1})(?=.*[a-z]{1})(?=.*[!@#$%^&*-]{1}).{7,20})/)
+  email: joi
+    .string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .messages({
+      "string.email": "Email is invalid",
+      "any.required": "Email is required",
+    }),
+
+  password: joi
+    .string()
+    // אות גדולה, אות קטנה, מספר, תו מיוחד, אורך 7–20
+    .ruleset.pattern(/((?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*-]).{7,20})/)
     .rule({
-        message: "password must be at least nine characters long and contain an uppercase letter, a lowercase letter, a number and one of the following characters !@#$%^&*-"
-    }).required(),
+      message:
+        "password must be 7–20 chars and include upper, lower, number and !@#$%^&*-",
+    })
+    .required(),
+
+  // ✅ זה מה שחסר וגרם לשגיאה "object.unknown"
+  rememberMe: joi.boolean().default(true),
 });
